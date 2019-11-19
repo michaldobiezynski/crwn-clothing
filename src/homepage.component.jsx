1b64d46a89c39a0c247c62e6f069797311cda4c2
michaldobiezynski <dobiezynski@gmail.com>
2019-11-19 19:46:11 +0000

    created basic structre for homepage, updated .gitignore and removed .idea

---
+++ src/homepage.component.jsx
@@ -0,0 +1,58 @@
+import React from 'react';
+
+const HomePage = () => (
+    <div className='homepage'>
+        <div className='directory-menu'>
+            <div className='menu-itm'>
+                <div className='content'>
+                    <h1 className='title'>
+                        HATS
+                        <span className='subtitle'>
+                            SHOP NOW
+                        </span>
+                    </h1>
+                </div>
+            </div>
+            <div className='menu-itm'>
+                <div className='content'>
+                    <h1 className='title'>
+                        JACKERS
+                        <span className='subtitle'>
+                            SHOP NOW
+                        </span>
+                    </h1>
+                </div>
+            </div>
+            <div className='menu-itm'>
+                <div className='content'>
+                    <h1 className='title'>
+                        SNEAKERS
+                        <span className='subtitle'>
+                            SHOP NOW
+                        </span>
+                    </h1>
+                </div>
+            </div>
+            <div className='menu-itm'>
+                <div className='content'>
+                    <h1 className='title'>
+                        WOMENS
+                        <span className='subtitle'>
+                            SHOP NOW
+                        </span>
+                    </h1>
+                </div>
+            </div>
+            <div className='menu-itm'>
+                <div className='content'>
+                    <h1 className='title'>
+                        MENS
+                        <span className='subtitle'>
+                            SHOP NOW
+                        </span>
+                    </h1>
+                </div>
+            </div>
+        </div>
+    </div>
+)
