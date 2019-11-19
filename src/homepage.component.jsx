011f787cd57af2a2c5b0c44221a3f1daf6d97fd5
michaldobiezynski <dobiezynski@gmail.com>
2019-11-19 20:09:14 +0000

    added the created styles to the homepage component

--- src/homepage.component.jsx
+++ src/homepage.component.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import './homepage.styles.scss';
 
 const HomePage = () => (
     <div className='homepage'>
         <div className='directory-menu'>
-            <div className='menu-itm'>
+            <div className='menu-item'>
                 <div className='content'>
                     <h1 className='title'>
                         HATS
