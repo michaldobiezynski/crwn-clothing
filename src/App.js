6ba8e162aeb985dbb07cff4a7ec114fabdbe4e7e
michaldobiezynski <dobiezynski@gmail.com>
2019-11-23 20:15:44 +0000

    sign

--- src/App.js
+++ src/App.js
@@ -6,12 +6,8 @@ import './App.css';
 import HomePage from './pages/homepage/homepage.component.jsx'
 import ShopPage from "./pages/shop/shop.component";
 import Header from './components/header/header.component';
+import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 
-const HatsPage = () => (
-    <div>
-        <h1>HATS PAGE </h1>
-    </div>
-);
 
 function App() {
     return <div>
@@ -19,6 +15,7 @@ function App() {
         <Switch>
             <Route exact path='/' component={HomePage} />
             <Route path='/shop' component={ShopPage} />
+            <Route path='/signin' component={SignInAndSignUpPage} />
         </Switch>
     </div>;
 }
