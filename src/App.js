76396a5eeba09331ce4160b224a67abd7017825d
michaldobiezynski <dobiezynski@gmail.com>
2019-12-06 20:05:15 +0000

    uupdating user object with redux in app component and using it in header

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-
+import { connect } from 'react-redux';
+import {setCurrentUser} from "./redux/user/user.actions";
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component.jsx'
@@ -12,33 +13,27 @@ import { createUserProfileDocument } from './firebase/firebase.utils';
 
 
 class App extends React.Component{
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            currentUser: null
-        }
-    }
 
     unsubscribeFromAuth = null;
 
     componentDidMount() {
+
+        const {setCurrentUser} = this.props;
+
         this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
 
         if (userAuth) {
             const userRef = await createUserProfileDocument(userAuth);
 
             userRef.onSnapshot(snapShot => {
-            this.setState({
-              currentUser: {
+            setCurrentUser({
                   id: snapShot.id,
                   ...snapShot.data()
-                    }
                 });
             });
             }
 
-        this.setState({currentUser: userAuth});
+        setCurrentUser({userAuth});
         });
     };
 
@@ -60,4 +55,8 @@ class App extends React.Component{
     }
 }
 
-export default App;
+const mapDispatchToProps = dispatch => ({
+    setCurrentUser: user => dispatch(setCurrentUser(user))
+});
+
+export default connect(null, mapDispatchToProps)(App);
