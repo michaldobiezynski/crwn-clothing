eb21123ea34f4154e93c33576583b0db07d08d25
michaldobiezynski <dobiezynski@gmail.com>
2019-12-16 12:42:28 +0000

    displaying cart items from the cart in the checkout component

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import { createStructuredSelector } from "reselect";
 import { selectCartItems, selectCartTotal } from "../../redux/cart/cart.selectors";
 import './checkout.styles.scss';
+import CheckoutItem from "../../components/checkout-item/checkout-item.component";
 
 const CheckoutPage = ({cartItems, total}) => (
     <div className='checkout-page'>
@@ -28,7 +29,7 @@ const CheckoutPage = ({cartItems, total}) => (
 
         {
             cartItems.map(cartItem =>
-            cartItem.name)
+            <CheckoutItem key={cartItem.id} cartItem={cartItem} /> )
         }
         <div className='total'>
             <span>TOTAL: ${total}</span>
