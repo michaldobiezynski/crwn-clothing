008338e880a21af090590925304f0f771c730003
michaldobiezynski <dobiezynski@gmail.com>
2019-12-26 12:29:16 +0000

    wired my application to pull shop data from firebase backend and also implemented higher order component in form of a spinner to indicate to users that a page is loading, I have also fixed some of the prolbems with add to cart buttons

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -64,7 +64,7 @@ export const convertCollectionsSnapshotToMap = (collections) => {
         }
     );
     return transformedCollection.reduce((accumulator, collection) => {
-        accumulator[collection.title.toLoaweCase()] = collection;
+        accumulator[collection.title.toLowerCase()] = collection;
         return accumulator;
     } , {});
 };
