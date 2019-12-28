d51fbceebde4d3b5157a73ab9d191413e8ba4899
michaldobiezynski <dobiezynski@gmail.com>
2019-12-28 19:43:12 +0000

    moved fetch collections async thunk into saga

--- src/redux/shop/shop.saga.js
+++ src/redux/shop/shop.saga.js
@@ -1,8 +1,22 @@
-import { takeEvery } from 'redux-saga/effects';
+import { takeEvery, call, put } from 'redux-saga/effects';
 import ShopActionTypes from "./shop.types";
+import {convertCollectionsSnapshotToMap, firestore} from "../../firebase/firebase.utils";
+import {fetchCollectionsFailure, fetchCollectionsSuccess} from "./shop.actions";
 
 export function* fetchCollectionsAsync() {
     yield console.log('I am fired');
+
+    try {
+
+        const collectionRef = firestore.collection('collections');
+        const snapshot = yield collectionRef.get();
+        const collectionsMap = yield call(convertCollectionsSnapshotToMap, snapshot);
+        yield put(fetchCollectionsSuccess(collectionsMap));
+
+    } catch (error) {
+        yield put(fetchCollectionsFailure(error.message));
+    }
+
 }
 
 export function* fetchCollectionsStart() {
