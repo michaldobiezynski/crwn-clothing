4a77ad8e53fa9b1b3435a0273370bd00d773505b
michaldobiezynski <dobiezynski@gmail.com>
2019-12-29 09:31:57 +0000

    some refactoring and moving email sign in into saga

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
-import {setCurrentUser} from "./redux/user/user.actions";
 import './App.css';
 import CheckoutPage from "./pages/checkout/checkout.component";
 import HomePage from './pages/homepage/homepage.component.jsx'
 import ShopPage from "./pages/shop/shop.component";
 import Header from './components/header/header.component';
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
-import {auth, createUserProfileDocument } from './firebase/firebase.utils';
 import { selectCurrentUser } from "./redux/user/user.selector";
 import { createStructuredSelector } from "reselect";
 
@@ -18,7 +16,6 @@ class App extends React.Component{
 
     componentDidMount() {
 
-        const {setCurrentUser} = this.props;
 
         // this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
         //
@@ -61,8 +58,6 @@ const mapStateToProps = createStructuredSelector({
     currentUser: selectCurrentUser
 });
 
-const mapDispatchToProps = dispatch => ({
-    setCurrentUser: user => dispatch(setCurrentUser(user))
-});
 
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+
+export default connect(mapStateToProps)(App);
