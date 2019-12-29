7e9565ad681eb3a210d3d8dfb17e87657026253c
michaldobiezynski <dobiezynski@gmail.com>
2019-12-29 12:11:30 +0000

    sign out with sagas

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -8,8 +8,9 @@ import {createStructuredSelector} from "reselect";
 import {selectCartHidden} from "../../redux/cart/cart.selectors";
 import {selectCurrentUser} from "../../redux/user/user.selector";
 import { HeaderContainer, LogoContainer, OptionsContainer, OptionLink } from "./header.styles";
+import { signOutStart } from "../../redux/user/user.actions";
 
-const Header = ({ currentUser, hidden }) => (
+const Header = ({ currentUser, hidden, signOutStart }) => (
     <HeaderContainer >
         <LogoContainer to="/">
             <Logo className='logo' />
@@ -23,7 +24,7 @@ const Header = ({ currentUser, hidden }) => (
             </OptionLink>
             {
                 currentUser ?
-                    <OptionLink as='div' onClick={() => auth.signOut()}>SIGN OUT</OptionLink>
+                    <OptionLink as='div' onClick={signOutStart}>SIGN OUT</OptionLink>
                     :
                     <OptionLink to='/signin'>SIGN IN</OptionLink>
             }
@@ -39,4 +40,8 @@ const mapStateToProps = createStructuredSelector({
     hidden: selectCartHidden
 });
 
-export default connect(mapStateToProps)(Header);
+const mapDispatchToProps = dispatch => ({
+    signOutStart: () => dispatch(signOutStart())
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Header);
