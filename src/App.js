262cc9551494f229a26a1bb2d501bfd9b305f81c
michaldobiezynski <dobiezynski@gmail.com>
2019-12-29 11:51:39 +0000

    recreating persistance with sagas

--- src/App.js
+++ src/App.js
@@ -9,30 +9,16 @@ import Header from './components/header/header.component';
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 import { selectCurrentUser } from "./redux/user/user.selector";
 import { createStructuredSelector } from "reselect";
+import { checkUserSession } from "./redux/user/user.actions";
 
 class App extends React.Component{
 
     unsubscribeFromAuth = null;
 
     componentDidMount() {
-
-
-        // this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
-        //
-        // if (userAuth) {
-        //     const userRef = await createUserProfileDocument(userAuth);
-        //
-        //     userRef.onSnapshot(snapShot => {
-        //     setCurrentUser({
-        //           id: snapShot.id,
-        //           ...snapShot.data()
-        //         });
-        //     });
-        //     }
-        // setCurrentUser(userAuth);
-        // });
-
-    };
+        const { checkUserSession } = this.props;
+        checkUserSession();
+    }
 
     componentWillUnmount() {
         this.unsubscribeFromAuth();
@@ -58,6 +44,8 @@ const mapStateToProps = createStructuredSelector({
     currentUser: selectCurrentUser
 });
 
+const mapDispatchToProps = dispatch => ({
+    checkUserSession: () => dispatch(checkUserSession())
+});
 
-
-export default connect(mapStateToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
